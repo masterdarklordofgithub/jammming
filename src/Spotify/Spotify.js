24ae5dbbcbde4b4e984bd02b33101fba11541385
masterdarklordoftheworld <adambostrom93@gmail.com>
2023-10-05 15:09:12 +0200

    wrap it up

--- src/Spotify/Spotify.js
+++ src/Spotify/Spotify.js
@@ -1,6 +1,7 @@
 
 //const client_secret = process.env.REACT_APP_CLIENT_SECRET;
-const clientId = process.env.REACT_APP_CLIENT_ID;
+//const clientId = process.env.REACT_APP_CLIENT_ID;
+const clientId = '3697fe787c2d44ff902128f9a1b77119';
 const redirectUri = 'https://651c887ed779950008d9fe91--adams-spotify-app.netlify.app/';
 const scope = 'playlist-modify-public playlist-modify-private';
 let accessToken;
